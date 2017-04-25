fbfb5fecb0cb9477942afacfcfcc3ec2c2fb2a4b
Ben <Wackbatt@users.noreply.github.com>
2017-04-25 19:44:33 +0100

    remoteexec

---
+++ CfgRemoteExec.hpp
@@ -0,0 +1,7 @@
+//add this in the client section 
+
+F(zipties_fnc_zip_tie,CLIENT)
+F(zipties_fnc_zip_gagplayer,CLIENT)
+F(zipties_fnc_zip_blindfold,CLIENT)
+F(zipties_fnc_zip_patdown,CLIENT)
+F(zipties_fnc_zip_untieFix,CLIENT)
